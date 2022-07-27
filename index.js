7d105453300a536947a5dcaa6338fd1e224b0d72
QunFan <allenfan1012@gmail.com>
2022-07-28 07:50:04 +1000

    Done.

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const currentUser = 'Allen';
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
 const excitedWelcomeMessage = `welcome to flatbook, ${currentUser}!`.toUpperCase();
-const shortGreeting = "Welcome, " + "A!";
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
